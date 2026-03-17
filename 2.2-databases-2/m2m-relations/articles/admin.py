8ced885a8e253bb34fb0076f07c232d3dc4829d6
Arthur Gukkaev <gukkaev2004@yandex.ru>
2026-03-17 17:20:08 +0300

    Задание 2: добавлены теги к статьям через промежуточную модель Scope

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,43 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        
+        main_count = 0
+        for form in self.forms:
+            # Пропускаем пустые формы и отмеченные на удаление
+            if not form.cleaned_data or form.cleaned_data.get('DELETE', False):
+                continue
+            
+            if form.cleaned_data.get('is_main', False):
+                main_count += 1
+        
+        if main_count == 0:
+            raise ValidationError('Укажите основной раздел')
+        if main_count > 1:
+            raise ValidationError('Основным может быть только один раздел')
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline]
+    list_display = ['title', 'published_at']
+    list_filter = ['published_at']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
-
-from articles.models import Article
-
+from .models import Article
 
 def articles_list(request):
-    template = 'articles/news.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
-
-    return render(request, template, context)
+    articles = Article.objects.all().prefetch_related('scopes__tag').order_by('-published_at')
+    return render(request, 'articles/news.html', {'object_list': articles}) 
